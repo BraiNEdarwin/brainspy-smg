62adf61c9eb07c17f0ae818f3ac38a0cc4925289
unai <u.alegre@utwente.nl>
2019-09-23 11:57:28 +0200

    Added basic structure

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(name='brainspy-algorithms',
+      version='0.0.0',
+      description='Automatisation for creating neural-network based surrogate models of boron-doped silicon chips.',
+      url='https://github.com/BraiNEdarwin/brainspy-smg',
+      author='This has adopted part of the BRAINS skynet repository code, which has been cleaned and refactored. The maintainers of the code are Hans-Christian Ruiz Euler and Unai Alegre Ibarra.',
+      author_email='u.alegre@utwente.nl',
+      license='GPL-3.0',
+      packages=find_packages(),
+      install_requires=[
+      ],
+      python_requires='~=3.6',
+      zip_safe=False)
