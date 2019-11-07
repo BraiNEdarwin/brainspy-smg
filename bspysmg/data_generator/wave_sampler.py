489567f682dbd3803ac33d565cdeb3a720782ad2
hcruiz <hans.ruiz.euler@hotmail.com>
2019-11-07 15:54:00 +0100

    work in progress: transient test separated from wave search into its own module

--- bspysmg/data_generator/wave_sampler.py
+++ bspysmg/data_generator/wave_sampler.py
@@ -77,36 +77,15 @@ def wave_sampler(configs):
     end_batch = time.time()
     print('Data collection for part ' + str(i + 1) + ' of ' + str(batches) + ' took ' + str(end_batch - start_batch) + ' sec.')
 
-if configs['transientTest']:
-    from SkyNEt.experiments.wave_search import transient_test
-    print("Testing for transients...")
-    print("Only for the last loaded data the transients are tested ")
-    ytestdata, difference, xtestdata = transient_test.transient_test(waves, data[(batches - 1) * configs['samplePoints:(batches) * configs['samplePoints], configs['sample_frequency, configs['sampleTime, configs['n)
-    SaveLib.saveExperiment(configs['configSrc, saveDirectory,
-                           xtestdata=xtestdata,
-                           ytestdata=ytestdata * configs['amplification / configs['postgain,
-                           diff=difference * configs['amplification / configs['postgain,
-                           output=data * configs['amplification / configs['postgain,
-                           freq=configs['freq,
-                           sampleTime=configs['sampleTime,
-                           sample_frequency=configs['sample_frequency,
-                           phase=configs['phase,
-                           amplitude=configs['amplitude,
-                           offset=configs['offset,
-                           amplification=configs['amplification,
-                           electrodeSetup=configs['electrodeSetup,
-                           gain_info=configs['gain_info,
-                           filename='training_NN_data')
-else:
-    SaveLib.saveExperiment(configs['configSrc, saveDirectory,
-                           output=data * configs['amplification / configs['postgain,
-                           freq=configs['freq,
-                           sampleTime=configs['sampleTime,
-                           sample_frequency=configs['sample_frequency,
-                           phase=configs['phase,
-                           amplitude=configs['amplitude,
-                           offset=configs['offset,
-                           amplification=configs['amplification,
-                           electrodeSetup=configs['electrodeSetup,
-                           gain_info=configs['gain_info,
-                           filename='training_NN_data')
+SaveLib.saveExperiment(configs['configSrc, saveDirectory,
+                        output=data * configs['amplification / configs['postgain,
+                        freq=configs['freq,
+                        sampleTime=configs['sampleTime,
+                        sample_frequency=configs['sample_frequency,
+                        phase=configs['phase,
+                        amplitude=configs['amplitude,
+                        offset=configs['offset,
+                        amplification=configs['amplification,
+                        electrodeSetup=configs['electrodeSetup,
+                        gain_info=configs['gain_info,
+                        filename='training_NN_data')
