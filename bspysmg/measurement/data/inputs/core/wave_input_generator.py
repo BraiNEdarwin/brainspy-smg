046720d4c691824c76e90ae73747912eeed3777f
ualegre <u.alegre@utwente.nl>
2019-11-08 16:57:45 +0100

    Major structural changes

--- bspysmg/measurement/data/inputs/core/wave_input_generator.py
+++ bspysmg/measurement/data/inputs/core/wave_input_generator.py
@@ -2,7 +2,7 @@
 """
 Created on Mon Dec 17 16:51:29 2018
 Script to sample a device using waves
-@author: M. Boon & HC Ruiz
+@author: M. Boon,  HC Ruiz, & Unai Alegre
 """
 
 # import save
@@ -36,82 +36,42 @@ def input_generator(points, configs):
 # initialize save directory
 
 
-def generate_triangle(self, freq, t, amplitude, fs, phase=np.zeros(7)):
-    '''
-    Generates a triangle wave form that can be used for the input data.
-    freq:       Frequencies of the inputs in an one-dimensional array
-    t:          The datapoint(s) index where to generate a sine value (1D array when multiple datapoints are used)
-    amplitude:  Amplitude of the sine wave (Vmax in this case)
-    fs:         Sample frequency of the device
-    phase:      (Optional) phase offset at t=0
-    '''
-    # There is an additional + np.pi/2 to make sure that if phase = 0. the inputs start at 0V
-
-    return signal.sawtooth((2 * np.pi * freq[:, np.newaxis] * t) / fs + phase[:, np.newaxis] + np.pi / 2, 0.5) * amplitude[:, np.newaxis]
-
-
-def generate_sinewave(self, freq, t, amplitude, fs, phase=np.zeros(7)):
-    '''
-    Generates a sine wave that can be used for the input data.
-    freq:       Frequencies of the inputs in an one-dimensional array
-    t:          The datapoint(s) index where to generate a sine value (1D array when multiple datapoints are used)
-    amplitude:  Amplitude of the sine wave (Vmax in this case)
-    fs:         Sample frequency of the device
-    phase:      (Optional) phase offset at t=0
-    '''
-
-    return np.sin((2 * np.pi * freq[:, np.newaxis] * t) / fs + phase[:, np.newaxis]) * amplitude[:, np.newaxis]
-
-
 def wave_sampler(configs):
     # Initialize output containers
-    data = np.zeros((int(configs['sampleTime'] * configs['sample_frequency']), 1))
+    data = np.zeros((int(configs['sample_time'] * configs['sample_frequency']), 1))
 
-    nr_batches = int(configs['sample_frequency'] * configs['sampleTime'] / configs['samplePoints'])
+    nr_batches = int(configs['sample_frequency'] * configs['sample_time'] / configs['sample_points'])
 
     for i in range(batches):
         start_batch = time.time()
 
-        time_points = np.arange(i * configs['samplePoints'], (i + 1) * configs['samplePoints'])
+        time_points = np.arange(i * configs['sample_points'], (i + 1) * configs['sample_points'])
         waves = input_generator(time_points, configs)
         # Use 0.5 second to ramp up to the value where data aqcuisition stopped previous iteration
         # and 0.5 second to ramp down after the batch is done
-        wavesRamped = np.zeros((waves.shape[0], waves.shape[1] + int(configs['sample_frequency'])))
-        dataRamped = np.zeros(wavesRamped.shape[1])
-        for j in range(wavesRamped.shape[0]):
-            wavesRamped[j, 0:int(0.5 * configs['sample_frequency'])] = np.linspace(0, waves[j, 0], int(0.5 * configs['sample_frequency']))
-            wavesRamped[j, int(0.5 * configs['sample_frequency']): int(0.5 * configs['sample_frequency']) + waves.shape[1]] = waves[j, :]
-            wavesRamped[j, int(0.5 * configs['sample_frequency']) + waves.shape[1]:] = np.linspace(waves[j, -1], 0, int(0.5 * configs['sample_frequency']))
+        waves_ramped = np.zeros((waves.shape[0], waves.shape[1] + int(configs['sample_frequency'])))
+        dataRamped = np.zeros(waves_ramped.shape[1])
+        for j in range(waves_ramped.shape[0]):
+            waves_ramped[j, 0:int(0.5 * configs['sample_frequency'])] = np.linspace(0, waves[j, 0], int(0.5 * configs['sample_frequency']))
+            waves_ramped[j, int(0.5 * configs['sample_frequency']): int(0.5 * configs['sample_frequency']) + waves.shape[1]] = waves[j, :]
+            waves_ramped[j, int(0.5 * configs['sample_frequency']) + waves.shape[1]:] = np.linspace(waves[j, -1], 0, int(0.5 * configs['sample_frequency']))
 
-        dataRamped = InstrumentImporter.nidaqIO.IO_cDAQ(wavesRamped, configs['sample_frequency'])
-        data[i * configs['samplePoints']: (i + 1) * configs['samplePoints'], 0] = dataRamped[0, int(0.5 * configs['sample_frequency']):int(0.5 * configs['sample_frequency']) + waves.shape[1]]
+        dataRamped = InstrumentImporter.nidaqIO.IO_cDAQ(waves_ramped, configs['sample_frequency'])
+        data[i * configs['sample_points']: (i + 1) * configs['sample_points'], 0] = dataRamped[0, int(0.5 * configs['sample_frequency']):int(0.5 * configs['sample_frequency']) + waves.shape[1]]
 
     if i % 10 == 0:  # Save after every 10 batches
         print('Saving...')
-        SaveLib.saveExperiment(configs['configSrc, saveDirectory,
+        SaveLib.saveExperiment(configs['config_src'], saveirectory,
                                output=data * configs['amplification'] / configs['postgain'],
                                freq=configs['freq'],
-                               sampleTime=configs['sampleTime'],
+                               sampleTime=configs['sample_time'],
                                sample_frequency=configs['sample_frequency'],
                                phase=configs['phase'],
                                amplitude=configs['amplitude'],
                                offset=configs['offset'],
                                amplification=configs['amplification'],
-                               electrodeSetup=configs['electrodeSetup'],
+                               electrodeSetup=configs['electrode_setup'],
                                gain_info=configs['gain_info'],
-                               filename='training_NN_data')
-    end_batch= time.time()
+                               filename='training_nn_data')
+    end_batch = time.time()
     print('Data collection for part ' + str(i + 1) + ' of ' + str(batches) + ' took ' + str(end_batch - start_batch) + ' sec.')
-
-SaveLib.saveExperiment(configs['configSrc, saveDirectory,
-                        output=data * configs['amplification / configs['postgain,
-                        freq=configs['freq,
-                        sampleTime=configs['sampleTime,
-                        sample_frequency=configs['sample_frequency,
-                        phase=configs['phase,
-                        amplitude=configs['amplitude,
-                        offset=configs['offset,
-                        amplification=configs['amplification,
-                        electrodeSetup=configs['electrodeSetup,
-                        gain_info=configs['gain_info,
-                        filename='training_NN_data')
