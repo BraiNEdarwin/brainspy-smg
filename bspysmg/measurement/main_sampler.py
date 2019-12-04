2b14da922b651d67d67bc6e09a6cd0c457895316
hcruiz <hans.ruiz.euler@hotmail.com>
2019-12-04 18:50:28 +0100

    minor changes, work in progress in main scripts and some cleanup

--- bspysmg/measurement/main_sampler.py
+++ bspysmg/measurement/main_sampler.py
@@ -1,12 +1,10 @@
 
 from bspyalgo.utils.io import load_configs
 from bspysmg.measurement.data.output.sampler_mgr import Sampler
-from bspysmg.measurement.data.processing import preprocess_data
+from bspysmg.measurement.data.processing.postprocessing import post_process
 
 CONFIGS = load_configs('configs/sampling/toy_sampling_configs_template.json')
 sampler = Sampler(CONFIGS)
 path_to_data = sampler.get_data()
 
-INPUTS, OUTPUTS, INFO_DICT = preprocess_data(path_to_data)
-
-## PLOTS? ##
+INPUTS, OUTPUTS, INFO_DICT = post_process(path_to_data)
