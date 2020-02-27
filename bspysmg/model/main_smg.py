cb5e770d4f4633fea7e633e9f6449d4a65523292
ualegre <u.alegre@utwente.nl>
2020-02-27 15:49:41 +0100

    Added automatic error adition to the info dictionary of the generated model. Added seed generation.

--- bspysmg/model/main_smg.py
+++ bspysmg/model/main_smg.py
@@ -6,7 +6,7 @@ from bspysmg.model.data.outputs import test_model
 from bspyalgo.utils.io import load_configs
 from bspyproc.utils.pytorch import TorchUtils
 
-TorchUtils.force_cpu = False
+TorchUtils.force_cpu = True
 
 configs = load_configs('./configs/training/test.json')
 
@@ -18,8 +18,8 @@ else:
 seed = TorchUtils.init_seed(seed, deterministic=True)
 configs['seed'] = seed
 
-# # # Get GD object with a processor specified in configs
-model_generator = get_algorithm(configs)
+# # # # Get GD object with a processor specified in configs
+model_generator = get_algorithm(configs, is_main=True)
 model_generator.save_smg_configs_dict()
 
 # Get training and validation data
@@ -32,14 +32,17 @@ plt.figure()
 plt.plot(LOSS)
 plt.title(f'Training profile')
 plt.legend(['training', 'validation'])
-plt.savefig(model_generator.dir_path + '/training_profile')
+plt.savefig(configs['results_base_dir'] + '/training_profile')
 
-np.savez(model_generator.dir_path + '/training_profile.npz', LOSS=LOSS)
+np.savez(configs['results_base_dir'] + '/summary.npz', LOSS=LOSS)
 # Test NN model with unseen test data
 
-TorchUtils.force_cpu = True
+#TorchUtils.force_cpu = True
 
-TEST_ERROR = test_model.get_error(model_generator.dir_path + '/trained_network.pt',
+# TEST_ERROR = test_model.get_error(model_generator.dir_path + '/trained_network.pt',
+#                                   model_generator.configs["data"]['test_data_path'])
+
+TEST_ERROR = test_model.get_error('tmp/output/model2020/1ep_1e-4lr_128mb_2020_02_13_180627/trained_network.pt',
                                   model_generator.configs["data"]['test_data_path'])
 
 model_generator.save_model_mse(TEST_ERROR)
