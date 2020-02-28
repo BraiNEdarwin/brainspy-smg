1ab6f1ddab57fad87ce1bb9008efb1def18f1a6b
ualegre <u.alegre@utwente.nl>
2020-02-28 13:13:15 +0100

    Separated code into plotting. Minor fix on the error initialisation.

--- bspysmg/model/data/outputs/test_model.py
+++ bspysmg/model/data/outputs/test_model.py
@@ -35,7 +35,7 @@ def get_error(model_path, test_data_path, steps=1, batch_size=2700000, model_nam
     with torch.no_grad():
         model = TorchModel({'torch_model_dict': model_path})
         INPUTS_TEST, TARGETS_TEST, INFO_DICT = load_data(test_data_path, steps)
-        error = np.zeros_like(INPUTS_TEST)
+        error = np.zeros_like(TARGETS_TEST)
         prediction = np.zeros_like(TARGETS_TEST)
     i_start = 0
     i_end = batch_size
@@ -48,50 +48,47 @@ def get_error(model_path, test_data_path, steps=1, batch_size=2700000, model_nam
         if i_end > threshold and i_end < INPUTS_TEST.shape[0]:
             i_end = INPUTS_TEST.shape[0]
 
-    MSE = np.mean(error**2)
-    print(f'MSE on Test Set of trained NN model: {MSE}')
+    mse = np.mean(error**2)
+    print(f'MSE on Test Set of trained NN model: {mse}')
     dir_path, model_name = os.path.split(model_path)
     model_name = os.path.splitext(model_name)[0]
-
-    model.info['data_info']['mse'] = MSE.item()
+    model.info['data_info']['mse'] = mse.item()
 
     path = create_directory(os.path.join(get_main_path(model_path), results_dir))
     save('numpy', os.path.join(path, 'error.npz'), error=error, prediction=prediction, targets=TARGETS_TEST)
     save('torch', os.path.join(path, model_name + '.pt'), timestamp=False, data=model)
+    plot_error_vs_output(TARGETS_TEST, error, path)
+    plot_error_hist(TARGETS_TEST, prediction, error, mse, path)
 
-    # PLOT PREDICTED VS TRUE VALUES and ERROR HIST
+
+def plot_error_hist(targets, prediction, error, mse, save_dir):
     plt.figure()
     plt.subplot(1, 2, 1)
-    plt.plot(TARGETS_TEST, prediction, '.')
+    plt.plot(targets, prediction, '.')
     plt.xlabel('True Output')
     plt.ylabel('Predicted Output')
-    targets_and_prediction_array = np.concatenate((TARGETS_TEST, prediction))
+    targets_and_prediction_array = np.concatenate((targets, prediction))
     min_out = np.min(targets_and_prediction_array)
     max_out = np.max(targets_and_prediction_array)
     plt.plot(np.linspace(min_out, max_out), np.linspace(min_out, max_out), 'k')
-    plt.title(f'Predicted vs True values:\n MSE {MSE}')
+    plt.title(f'Predicted vs True values:\n MSE {mse}')
     plt.subplot(1, 2, 2)
     plt.hist(np.reshape(error, error.size), 500)
     x_lim = 0.25 * np.max([np.abs(error.min()), error.max()])
     plt.xlim([-x_lim, x_lim])
     plt.title('Scaled error histogram')
-    fig_loc = os.path.join(dir_path, f'Test_Error_{model_name}')
-    plt.savefig(fig_loc)
-    print(f'Test Error Figures saved in {dir_path}')
-    # PLOT TEST ERROR VS OUTPUT
+    fig_loc = os.path.join(save_dir, 'test_error')
+    plt.savefig(fig_loc, dpi=300)
+    plt.close()
+
+
+def plot_error_vs_output(targets, error, save_dir):
     plt.figure()
-    plt.plot(TARGETS_TEST, error, '.')
-    plt.plot(np.linspace(TARGETS_TEST.min(), TARGETS_TEST.max(), len(error)), np.zeros_like(error))
+    plt.plot(targets, error, '.')
+    plt.plot(np.linspace(targets.min(), targets.max(), len(error)), np.zeros_like(error))
     plt.title('Error vs Output')
     plt.xlabel('Output')
     plt.ylabel('Error')
-    fig_loc = os.path.join(dir_path, f'TestError_vs_Output_{model_name}')
-    plt.savefig(fig_loc)
-    print(f'Test Error Figures saved in {dir_path}')
-
-    return MSE
-
-
-if __name__ == '__main__':
-    error = get_error('/home/hruiz/Documents/PROJECTS/DARWIN/Data_Darwin/Devices/training_1080batches_50s_50Hz_2019_12_05_172238/model_1200ep_10-4lr_mb128_2019_12_10_170729/trained_network.pt',
-                      '/home/hruiz/Documents/PROJECTS/DARWIN/Data_Darwin/Devices/BRAINSPY_DATA/training_1080batches_50s_50Hz_NonZeroPhase_2019_12_10_164417/training_data.npz')
+    fig_loc = os.path.join(save_dir, 'test_error_vs_output')
+    plt.savefig(fig_loc, dpi=300)
+    plt.close()
