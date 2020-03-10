99e39437b2c91340d31a6ba07fe40f92be20f115
hcruiz <hans.ruiz.euler@hotmail.com>
2020-03-10 13:42:54 +0100

    tested main_smg.py; the training and test phases are separated in two functions. This is intended as a template of the workflow/standard use case.

--- bspysmg/model/data/outputs/test_model.py
+++ bspysmg/model/data/outputs/test_model.py
@@ -1,10 +1,10 @@
 import numpy as np
-import matplotlib.pyplot as plt
 import torch
 import os
-from bspyalgo.utils.io import save, create_directory
+from bspyalgo.utils.io import create_directory
 from bspysmg.model.data.plots.model_results_plotter import plot_all
 from bspyproc.utils.pytorch import TorchUtils
+from bspyproc.processors.simulation.surrogate import SurrogateModel
 
 
 def load_data(path, steps):
@@ -33,28 +33,33 @@ def get_previous_path(test_data_path):
     return path[-2]
 
 
-def get_error(model, model_data_path, test_data_path, steps=1, batch_size=2700000, model_name='trained_network_with_mse'):
+def get_error(model_data_path, test_data_path, steps=1, batch_size=2048):
+
+    inputs, targets, info = load_data(test_data_path, steps)
+    error = np.zeros_like(targets)
+    prediction = np.zeros_like(targets)
+    model = SurrogateModel({'torch_model_dict': model_data_path})
     with torch.no_grad():
-        inputs, targets, info = load_data(test_data_path, steps)
-        amplification = np.array(info['processor']['amplification'])
-        error = np.zeros_like(targets)
-        prediction = np.zeros_like(targets)
-        #inputs = TorchUtils.get_tensor_from_numpy(inputs)
-        # targets = TorchUtils.get_tensor_from_numpy(targets)
-
-    i_start = 0
-    i_end = batch_size
-    threshold = (inputs.shape[0] - batch_size)
-    while i_end < inputs.shape[0]:
-        prediction[i_start:i_end] = TorchUtils.get_numpy_from_tensor(model(TorchUtils.get_tensor_from_numpy(inputs[i_start:i_end]))) * amplification
-        error[i_start:i_end] = prediction[i_start:i_end] - targets[i_start:i_end]
-        i_start += batch_size
-        i_end += batch_size
-        if i_end > threshold and i_end < inputs.shape[0]:
-            i_end = inputs.shape[0]
-
-    path = create_directory(os.path.join(get_main_path(model_data_path), get_previous_path(test_data_path)))
-    mse = plot_all(targets, prediction, path, name='TEST')
-    # save('numpy', os.path.join(path, 'error.npz'), error=TorchUtils.get_numpy_from_tensor(error), prediction=TorchUtils.get_numpy_from_tensor(prediction), targets=TorchUtils.get_numpy_from_tensor(targets), test_mse=TorchUtils.get_numpy_from_tensor(mse))
+        i_start = 0
+        i_end = batch_size
+        threshold = (inputs.shape[0] - batch_size)
+        while i_end <= inputs.shape[0]:
+            prediction[i_start:i_end] = TorchUtils.get_numpy_from_tensor(model(TorchUtils.get_tensor_from_numpy(inputs[i_start:i_end])))
+            error[i_start:i_end] = prediction[i_start:i_end] - targets[i_start:i_end]
+            i_start += batch_size
+            i_end += batch_size
+            if i_end > threshold and i_end < inputs.shape[0]:
+                i_end = inputs.shape[0]
+        main_path = os.path.dirname(os.path.dirname(model_data_path))
+        path = create_directory(os.path.join(main_path, 'test_model'))
+        mse = plot_all(targets, prediction, path, name='TEST')
 
     return mse
+
+
+if __name__ == "__main__":
+
+    model_path = '/home/hruiz/Documents/PROJECTS/DARWIN/Data_Darwin/Devices/BRAINSPY_DATA/BRAINS/Brains_2020_03_06_182728/BRAINS_500ep_0.00005lr_mb128_2020_03_09_213229/reproducibility/model.pt'
+    test_data_path = '/home/hruiz/Documents/PROJECTS/DARWIN/Data_Darwin/Devices/BRAINSPY_DATA/BRAINS/Brains_phase1_2020_03_09_094534/postprocessed_data.npz'
+    mse = get_error(model_path, test_data_path)
+    print(f'MSE: {mse}')
--- bspysmg/model/main_smg.py
+++ bspysmg/model/main_smg.py
@@ -2,11 +2,11 @@
 from bspyalgo.utils.io import load_configs
 from bspysmg.model.data.outputs import test_model
 from bspysmg.model.data.outputs.train_model import train_surrogate_model
-# TorchUtils.force_cpu = True
 
 configs = load_configs('configs/training/smg_configs_template.json')
 
 model_generator = train_surrogate_model(configs)
+
 # Test NN model with unseen test data
-test_model.get_error(model_generator.path_to_model,
-                     model_generator.configs["data"]['test_data_path'], batch_size=512)
+mse = test_model.get_error(model_generator.path_to_model, model_generator.configs["data"]['test_data_path'])
+print(f'MSE: {mse}')
