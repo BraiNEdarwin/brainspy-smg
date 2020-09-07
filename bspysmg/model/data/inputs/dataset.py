c5c4cd7bb0a89cf8d519704f055b43c05d65152f
ualegre <u.alegre@utwente.nl>
2020-09-07 13:54:43 +0200

    Minor fix on the configurations for the data in the dataset

--- bspysmg/model/data/inputs/dataset.py
+++ bspysmg/model/data/inputs/dataset.py
@@ -9,8 +9,7 @@ from brainspy.utils.pytorch import TorchUtils
 class ModelDataset(Dataset):
 
     def __init__(self, configs):
-        path = os.path.join(configs["results_base_dir"], configs["data"]['postprocessed_data_path'])
-        self.inputs, targets, self.info_dict = self.load_data(path, configs["data"]['steps'])
+        self.inputs, targets, self.info_dict = self.load_data(configs["data"])
         self.targets = targets / self.info_dict['processor']['driver']['amplification']
         assert len(self.inputs) == len(self.targets), 'Inputs and Outpus have NOT the same length'
 
@@ -20,15 +19,15 @@ class ModelDataset(Dataset):
     def __getitem__(self, index):
         return (self.inputs[index, :], self.targets[index, :])
 
-    def load_data(self, path, steps):
-        print('Data loading from: \n' + path)
-        with np.load(path, allow_pickle=True) as data:  # why was allow_pickle not required before? Do we need this?
+    def load_data(self, configs):
+        print('Data loading from: \n' + configs['postprocessed_data_path'])
+        with np.load(configs['postprocessed_data_path'], allow_pickle=True) as data:  # why was allow_pickle not required before? Do we need this?
             # TODO: change in data generation the key meta to info_dictionary
             info_dictionary = data['info'].tolist()
             print(f'Metadata :\n {info_dictionary.keys()}')
             # Create from numpy arrays torch.tensors and send them to device
-            inputs = TorchUtils.get_tensor_from_numpy(data['inputs'][::steps])  # shape: Nx#electrodes
-            outputs = TorchUtils.get_tensor_from_numpy(data['outputs'][::steps])  # Outputs need dim Nx1
+            inputs = TorchUtils.get_tensor_from_numpy(data['inputs'][::configs['steps']])  # shape: Nx#electrodes
+            outputs = TorchUtils.get_tensor_from_numpy(data['outputs'][::configs['steps']])  # Outputs need dim Nx1
             print(f'Shape of outputs: {outputs.shape}; shape of inputs: {inputs.shape}')
 
         return inputs, outputs, info_dictionary
