d5b67422dfa1c6476ba2c0039d7abf0608da75ab
ualegre <u.alegre@utwente.nl>
2021-03-26 11:48:20 +0100

    Updated version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='brainspy-smg',
-      version='0.8.1',
+      version='0.8.2',
       description='Automatisation for creating neural-network based surrogate models of boron-doped silicon chips.',
       url='https://github.com/BraiNEdarwin/brainspy-smg',
       author='This has adopted part of the BRAINS skynet repository code, which has been cleaned and refactored. The maintainers of the code are Hans-Christian Ruiz Euler and Unai Alegre Ibarra.',
