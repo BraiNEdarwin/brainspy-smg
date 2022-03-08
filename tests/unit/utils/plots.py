f3fd246aeaf3de2b585e2b77be976544cf6c3c06
Srikumar Sastry <s.s.sastry@student.utwente.nl>
2022-03-08 17:38:19 +0100

    Plots Testing

--- tests/unit/utils/plots.py
+++ tests/unit/utils/plots.py
@@ -23,6 +23,32 @@ class Test_Plots(unittest.TestCase):
         with self.assertRaises(TypeError):
             plots.plot_error_hist()
         
+        with self.assertRaises(AssertionError):
+            plots.plot_error_hist(target, predictions, np.array([0]), mse, ".")
+        
+        with self.assertRaises(AssertionError):
+            plots.plot_error_hist(target, predictions, error, -1, ".")
+    
+
+    def test_error_output(self):
+
+        target = np.random.uniform(0, 1, size=100)
+        error = np.random.uniform(0, 1, size=100)
+
+        try:
+            plots.plot_error_vs_output(target, error, ".")
+        except:
+            self.fail("Fail in creating error hist")
+        
+        with self.assertRaises(AttributeError):
+            plots.plot_error_vs_output(list(target), list(error), ".")
+        
+        with self.assertRaises(TypeError):
+            plots.plot_error_vs_output()
+        
+        with self.assertRaises(AssertionError):
+            plots.plot_error_vs_output(target, np.array([0]), ".")
+        
 
 if __name__ == '__main__':
     unittest.main()
