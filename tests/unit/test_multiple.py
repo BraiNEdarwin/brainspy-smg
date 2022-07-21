aa6eea0a0912cde1976c6212091e4f59e286373e
ualegre <u.alegre@utwente.nl>
2022-07-21 16:22:08 +0200

    Finished tests for multiple iv curves

---
+++ tests/unit/test_multiple.py
@@ -0,0 +1,36 @@
+import unittest
+from bspysmg.utils.iv import multiple
+import numpy as np
+from bspysmg import TEST_MODE
+from brainspy.utils.io import load_configs
+from bspysmg.utils import plots
+
+class Test_Multiple(unittest.TestCase):
+    def __init__(self, *args, **kwargs) -> None:
+        super(Test_Multiple, self).__init__(*args, **kwargs)
+        self.configs = load_configs("configs/utils/brains_ivcurve_template.yaml")
+
+    @unittest.skipUnless(TEST_MODE == "HARDWARE_CDAQ"
+                         or TEST_MODE == "HARDWARE_NIDAQ",
+                         "Hardware test is skipped for simulation setup.")
+    def test_init(self):
+
+        try:
+            self.configs['driver']['instruments_setup']['A']['activation_channel_mask'] = [1,1,1,1,1,1,1]
+            measurement = multiple.MultiIVMeasurement(self.configs)
+            configs, inputs, output = measurement.run_test(close_driver=False)
+            self.configs['input_signal']['input_signal_type'] = "sine"
+            configs, inputs, output = measurement.run_test(close_driver=True)
+            self.configs['driver']['instruments_setup']['A']['activation_channel_mask'] = [1,1,0,1,1,1,1]
+            measurement = multiple.MultiIVMeasurement(self.configs)
+            configs, inputs, output = measurement.run_test(close_driver=True)
+            plots.multi_iv_plot(configs, inputs, output, show_plot=False)
+        except Exception:
+            self.fail("Failed in creating error hist")
+
+
+if __name__ == '__main__':
+    import bspysmg
+
+    bspysmg.TEST_MODE = 'HARDWARE_CDAQ'
+    unittest.main()
--- tests/unit/test_simple.py
+++ tests/unit/test_simple.py
@@ -7,8 +7,6 @@ from brainspy.utils.io import load_configs
 class Test_Simple(unittest.TestCase):
     def __init__(self, *args, **kwargs) -> None:
         super(Test_Simple, self).__init__(*args, **kwargs)
-        import os 
-        print(os.getcwd())
         self.configs = load_configs("configs/utils/brains_ivcurve_template_simple.yaml")
 
     @unittest.skipUnless(TEST_MODE == "HARDWARE_CDAQ"
